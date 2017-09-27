26d06cf76c1574312956fb3899f55c45a14de7a9
B0Dim <B0Dim@me.com>
2017-09-27 12:57:15 +1000

    Initial Commit

---
+++ DeliveryFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeliveryFood
+//
+//  Created by B0Dim on 27.09.17.
+//  Copyright © 2017 B0Dim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
